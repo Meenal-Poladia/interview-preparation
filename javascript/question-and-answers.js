6e2bacacfe57115e8655065d0a7f0e008489d943
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-12-19 16:01:31 -0500

    Added more interview questions

--- javascript/question-and-answers.js
+++ javascript/question-and-answers.js
@@ -1,7 +1,4 @@
-/*
-
-
-    What is Javascript?
+/* Question 1: What is Javascript?
     JavaScript is a versatile programming language commonly used for creating interactive and dynamic content on the
     web. It is a high-level, interpreted language that enables developers to manipulate the DOM.
 
@@ -20,9 +17,9 @@
     execution of other code.
     5. Cross-Platform: JavaScript is not limited to web browsers. It can also help to develop server-side development,
     using NodeJs allowing developers to create full-stack applications using a single language.
+*/
 
-
-    Which are the types of operators in javascript?
+/* Question 2: Types of operators in javascript?
     1. Arithmetic Operators
     2. Comparison (Relational) Operators
     3. Bitwise Operators
@@ -30,8 +27,9 @@
     5. Assignment Operators
     6. Special Operators
 
+*/
 
-    Which are the datatype in Javascript?
+/* Question 3: Datatypes in Javascript?
     1. Primitive data type - Bunns
         ■	String - represents sequence of characters e.g. "hello"
         ■	Number - represents numeric values e.g. 100
@@ -43,8 +41,9 @@
         ■	Array - represents group of similar values
         ■	RegExp - represents regular expression
 
+*/
 
-    Difference between undefined and null?
+/* Question 4: Difference between undefined and null?
     Null and undefined are primitive data type values. They both are falsy values.
 
     Undefined means the default value of a variables that are declared but not assigned a value. Function parameters
@@ -56,8 +55,9 @@
     If we don’t assign any value to variable javascript automatically assigns it undefined value and if we assign
     a variable as null it means that we are setting the variable empty.
 
+*/
 
-    What is type conversion and type coercion?
+/* Question 5: Type conversion and type coercion?
     Type conversion is when we manually convert a variable from one data type to another. eg: To manually convert
     strings to numbers, use the Number() constructor
 
@@ -67,6 +67,84 @@
     + coerces to string
     > and - and *  and / coerces to number
 
+*/
 
+/* Question 6: Truthy values and falsy values? Falsy: neun0F
+    In JS there are 5 falsy values 0, '', undefined, null, NaN and of course false itself. These values are not false
+    initially but will be converted to false when converting them to boolean.
+
+    Objects are truthy values.
+
+    JS performs type coercion to booleans in two cases:
+        1. when using operators.
+        2. when using if blocks.
+*/
+
+/* Question 7: Types of Comment
+
+Single line comment in javascript is //
+Multiline comment in javascript
+
+ */
+
+/* Question 8: Let, const and var
+    These are 3 ways of declaring variables in JS. Let and const were introduced in ES6, and so they are modern
+    JavaScript. While the var keyword is the old way of declaring variables.
+
+    1. Let
+    - It is Block scoped variable.
+    - When declaring a variable using let, we can reassign a value to the variable or mutate its value.
+    - We also use let when we want to declare a variable but assign a value later to it.
+
+    2. Const
+    - It is also a Block scoped variable.
+    - When declaring a variable using the const keyword, the value of the variable cannot be changed.
+    - Unlike let we cannot declare a const variable and assign it a value later. Its value is immutable.
+
+    3. Var.
+    - This is the old way of declaring variables.
+    - Like let we can declare a variable using the var keyword and assign it a value later.
+
+ */
+
+/* Question 9: Prefer Let and Const over Var
+
+    1. Block Scoping
+    Variables declared with let and const have block-level scope, meaning they are only accessible within the block
+    (enclosed by curly braces) in which they are defined.
+    var has function-level scope, leading to potential issues with variable hoisting and unintended access outside of
+    blocks.
+
+    2. Reassignment (Mutability)
+    Variables declared with const cannot be reassigned after initialization. This helps prevent accidental
+    reassignments and makes the code more predictable.
+    let allows for reassignment, providing flexibility when the variable's value needs to change.
+
+    3. Global Object Property
+    Variables declared with var at the global scope become properties of the global object (window in browsers).
+    This can lead to unintended global variable pollution.
+    let and const declarations at the global scope do not create properties on the global object.
+
+    4. Temporal Dead Zone (TDZ)
+    let and const variables are in the "Temporal Dead Zone" from the start of the block until the actual declaration.
+    This helps catch potential issues related to accessing variables before they are declared.
+    var does not have this Temporal Dead Zone behavior, which can lead to subtle bugs.
+
+ */
+
+/* Question 10: What is anonymous function?
+    An anonymous function is a function without a name. An anonymous function is often not accessible after its
+    initial creation.
+    Eg. let show = function () { console.log('Anonymous function');}; show()
+
+ */
+
+/* Question 11: Async and Defer Javascript
+
+    When using async in <script> tag as <script async>, the browser will download the file and do the HTML
+    parsing at the same time. Once the download completed, it will pause the HTML execution and run the script.
+
+    Now if we use defer as <script defer>, the browser downloads the script during the HTML parsing, but only execute
+    the script after the parser has completed.
 
  */

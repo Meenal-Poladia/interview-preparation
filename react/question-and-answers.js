81041873a4e82e521a4324155f79a052164d169b
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-12-21 13:14:59 -0500

    Added more react interview questions

--- react/question-and-answers.js
+++ react/question-and-answers.js
@@ -64,6 +64,32 @@
 
  */
 
-/* Question 7:
+/* Question 7: What are Functional and Class-based components
+    Functional components and class components are two ways to define React components.
+    Function Components: Function components are simply JavaScript functions that receive props as input and return
+    React elements. They are also known as stateless components. They are defined using the "function" keyword
+    followed by the component name and the props argument. Function components do not have access to lifecycle
+    methods or the state.
+
+    Class Components: Class components are created using the "class" keyword, and they extend the React.Component
+    class. They have a state, lifecycle methods, and can be used when you need to manage state or make use of lifecycle
+    methods within your component. Class components are also known as stateful components.
+
+ */
+
+/* Question 8: Difference between Functional and Class-based components
+    1. Function components are simple, lightweight and fast, while class components are more complex and slow.
+    2. Function components do not have access to lifecycle methods or state, while class components do.
+    3. Function components are easier to read and understand, while class components can get complex quickly, especially
+    if you are using lifecycle methods and state.
+
+ */
+
+/* Question 9: What is the virtual DOM? How does react use the virtual DOM to render the UI?
+    Virtual DOM is a concept where a virtual representation of the real DOM is kept inside the memory and is synced
+    with the real DOM by a library such as ReactDOM.
+
+    Whenever there is a change in any component the entire code is not rendered again but only the component changed
+    and its children are rendered which makes react optimised than other framework.
 
  */

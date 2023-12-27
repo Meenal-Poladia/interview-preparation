876e67d9bfaa41c68c4dd83a3f91f3c92bbb2d67
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-12-27 12:17:06 -0500

    Added more javascript interview questions

--- javascript/question-and-answers.js
+++ javascript/question-and-answers.js
@@ -768,6 +768,106 @@ let map = new Map(newArray);
 
  */
 
+/* Question 54: Useful String methods
+
+    toUpperCase(), toLowerCase() -              Converts the string to upper/ lower case
+
+    replace(), replaceAll() -                   Replaces one/ all occurrences of the passed substr with the newSubstr
+    Params - substr, newSubstr                  replacer string
+
+    split() -                                   Generates an array of strings by breaking up the string using the
+    Params - separator                          passed separator parameter
+
+    substring(), slice() -                      Returns the part of the string between the start(inclusive) and
+    Params - startIndex, endIndex               end(excluding) indexes, or to the end of the string.
+
+    repeat() -                                  Constructs and returns a new string which contains the specified number
+    Params - count                              of copies
+
+    indexOf(), lastIndexOf() -                  Returns the first/ last index of passed searchValue in current string
+    Params - searchValue
+
+    padStart(), padEnd() -                      Pads the current string with the padString parameter (repeated,
+    Params - targetLength, padString            if needed) so that the resulting string reaches a given length.
+                                                The padding is applied from the start/ end of the current string.
+
+    includes(), startsWith(), endsWith() -      Returns a boolean notifying if the string contains/ starts with/
+    Params - substring                          ends with the passed substring parameter
+
+    trim(), trimStart(), trimEnd() -            The trim/ trimStart/ trimEnd methods removes whitespace from
+                                                everywhere/ beginning/ end of a string.
+ */
+
+/* Question 55: Useful Array methods
+
+    slice() -                                   Returns returns a shallow copy of a portion of an array into a new
+    Params - startIndex, endIndex               array object  between the start(inclusive) and
+                                                end(excluding) indexes, or to the end of the array.
+                                                Does not mutate array.
+
+    splice() -                                  Changes the contents of an array by removing or replacing existing
+    Params - start, deleteCount                 elements
+
+    reverse() -                                 Reverses an array in place. The first array element becomes the last,
+                                                and the last array element becomes the first.
+
+    concat() -                                  Merges two or more arrays. This method returns a new array.
+    Params - value0, value1, ...
+
+    join() -                                    Returns a new string by concatenating all of the elements in an array
+    Params - separator                          separated by commas(default) or a specified separator string
+
+    map() -                                     Creates a new array populated with the results of calling a provided
+    Params - mapper callback                    function on every element in the calling array
+
+    filter() -                                  Creates a new array with all elements that pass the test
+    Params - filter callback                    implemented by the provided function
+
+
+    reduce() -                                  Executes a user-supplied “reducer” callback function on each element of
+    Params - reducer callback, accumulator      the array, passing in the return value from the calculation on the
+                                                preceding element. The final result of running the reducer across all
+                                                elements of the array is a single value.
+
+    push()                                      Adds one or more elements to the end of an array and
+    Params - ...elements                        returns the new length of the array.
+
+    pop() -                                     Removes the last element from the array and returns it
+
+    unshift() -                                 Adds one or more elements to the beginning of an array and
+    Params - ...elements                        returns the new length of the array.
+
+    shift() -                                   Removes the first element from an array and returns that removed element.
+                                                This method changes the length of the array.
+
+
+    indexOf(), lastIndexOf() -                  Returns the first/ last index of passed searchElement in current array
+    Params - searchElement, fromIndex?          and returns -1 if not found. Tests for equality.
+
+    find(), findIndex() -                       Returns the value/ index of the first element in the provided array
+    Params - find callback                      that satisfies the provided find function. Tests for condition
+
+    includes() -                                Determines whether an array includes a certain value among its entries,
+    Params - searchElement, fromIndex?          returning true or false as appropriate. Tests for equality.
+
+    some(), every() -                           Tests whether at least one/ every element(s) in the array pass the test
+    Params - search callback                    implemented by the provided function. It returns true if, in the array,
+                                                it finds an element for which the provided function returns true;
+                                                otherwise it returns false. It doesn't modify the array.
+                                                Tests for condition
+
+    flat() -                                    Creates a new array with all sub-array elements concatenated into it
+    Params - depth                              recursively up to the specified depth.
+
+
+    flatMap() -                                 Returns a new array formed by applying a given callback function to
+    Params - map callback                       each element of the array, and then flattening the result by one level.
+
+    sort() -                                    Sorts the elements of an array in place and returns the sorted array.
+    Params - sort callback                      The sort callback helps to sort non string or complex values.
+
+ */
+
 // Promises in JS
 
 // Async /Await

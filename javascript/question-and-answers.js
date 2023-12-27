3c87e8cdeb36fae8aa9416bc9ef434dbbd78ee52
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-12-27 12:14:51 -0500

    Added more javascript interview questions

--- javascript/question-and-answers.js
+++ javascript/question-and-answers.js
@@ -713,6 +713,61 @@ let map = new Map(newArray);
 
  */
 
+/* Question 51: Build process
+    The build process is complex and it will almost always involve the following steps apart from others:
+
+    1. Bundling - In this we combine all our modules together into one big file. This is a pretty complex process which
+    can eliminate unused code and compress our code as well. This step is important for two big reasons. First,
+    older browsers don't support modules at all so code that's in a module cannot be executed by any older browser.
+    Second, it's better for performance to send less files to the browser to download and execute.
+
+    2. Transpiling & Polyfilling - In the second step we do something called transpiling and polyfilling, which is
+    basically converting all modern JavaScript syntax and features back to old ES5 syntax so that even older browsers
+    can understand our code without breaking. This step is usually done using a tool called Babel.
+
+    We don't perform these steps ourselves. Instead, we use a special tool to implement this build process for us.
+    The most common build tools available, are probably webpack and parcel. They are called bundlers.
+
+ */
+
+/* Question 52: Object traversal and looping
+
+    For keys:                   Object.keys(myObj) and then for...of loop
+    For values:                 Object.values(myObj) and then for...of loop
+    For both keys and values:   Object.entries(myObj) and then for...of loop
+    for...in:                   for(const property in obj) console.log(property + obj[property])
+
+ */
+
+/* Question 53: Useful Object methods
+
+    Object.assign(target, ...sources) - Copies the values of all enumerable own properties from one or
+                                        more source objects to a target object. Creates a shallow copy
+                                        of the object.
+
+    Object.create(proto) -              Creates a new object with the specified prototype object and properties.
+
+    Object.entries(obj) -               Returns an array containing all of the [key, value] pairs of a given
+                                        object's own enumerable string properties.
+
+    Object.keys(obj) -                  Returns an array containing the names of all of the given object's
+                                        own enumerable string properties.
+
+    Object.values(obj) -                Returns an array containing the values that correspond to all of a
+                                        given object's own enumerable string properties.
+
+    Object.freeze(obj) -                Freezes an object. A frozen object can no longer be changed. Freezing an
+                                        object prevents new properties from being added to it and existing properties
+                                        from being removed. Only shallow freezes an object.
+
+    Object.getOwnPropertyNames(obj) -   Returns an array containing the names of all of the given object's
+                                        own enumerable and non-enumerable properties.
+
+    Object.defineProperty(obj, prop, -  Adds the named property described by a given descriptor to an object.
+    descriptor)                         Can be used to add enumerable and non enumerable property to the object.
+
+ */
+
 // Promises in JS
 
 // Async /Await

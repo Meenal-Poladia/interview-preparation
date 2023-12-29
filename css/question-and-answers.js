53f7f0526e8593a762884fc2e61ccf77801da0aa
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-12-29 10:41:21 -0500

    Added more css interview questions

---
+++ css/question-and-answers.js
@@ -0,0 +1,49 @@
+
+/* Question 1: What is CSS
+    CSS stands for Cascading Style Sheet. It is a popular styling language which is used with HTML to design websites.
+
+ */
+
+/* Question 2: How can you integrate CSS on a web page
+    There are three methods to integrate CSS on web pages.
+        1. Inline method - It is used to insert style sheets in HTML document
+        2. Embedded/Internal method - It is used to add a unique style to a single document
+        3. Linked/Imported/External method - It is used when you want to make changes on multiple pages.
+
+ */
+
+/* Question 3: Some CSS frameworks
+    1. Bootstrap
+    2. Foundation
+    3. Semantic UI
+    4. Gumby
+    5. Ulkit
+
+ */
+
+/* Question 4: Different types of Selectors in CSS
+    Universal Selector, Element type Selector, ID selector, Class selector, Descendant combinatory, Child Combinator,
+    General Sibling Combinator, Adjacent sibling combinator, and Attribute selector.
+ */
+
+/* Question 5: What is a CSS Preprocessor? What are Sass, Less, and Stylus? Why do people use them?
+    CSS preprocessor is a tool used to enhance the basic functionality and let us use the complex logical syntax such
+    as variables, functions, mixins, and code nesting within vanilla CSS scripts themselves.
+
+        1. Sass (Syntactically Awesome Style Sheets) uses .sass extension. It is used for indentation; it doesn’t use
+        semicolons or curly brackets.
+        2. Less (Leener Stylesheets) uses .less extension. It is easy to add to any JavaScript Project by using NPM or
+        less.js file. Here, @ is used to define the variables.
+        3. Stylus provides great flexibility in writing syntax. It is able to use native CSS as well as the exclusion of
+        brackets, colons, and semicolons. There is no need to use @ or $ to define the variables.
+    People use SASS, LESS, and Stylus in order to extend the basic functionality of vanilla CSS.
+
+ */
+
+/* Question 6: Difference between reset vs normalize CSS? How do they differ
+    1. Reset CSS is used to remove all built-in styles in the browser such as paddings, margins, and font sizes, and
+    can be reset using all the elements.
+    2. Normalize CSS is used to make all built-in styles in the browser consistent and correct bugs as per varying
+    browsers.
+
+ */

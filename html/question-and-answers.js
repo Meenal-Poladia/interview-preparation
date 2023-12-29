1d3a46f68047f0c42e6dd38d5372b26898969317
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-12-29 12:02:25 -0500

    Added html interview questions

---
+++ html/question-and-answers.js
@@ -0,0 +1,35 @@
+/* Question 1: What is HTML?
+    HTML stands for Hypertext Markup Language. It is the standard markup language for creating web pages and web
+    applications.
+
+ */
+
+/* Question 2: Purpose of DOCTYPE in HTML?
+    The <!DOCTYPE> declaration specifies the HTML version being used and helps browsers render the document correctly.
+
+ */
+
+/* Question 3: Semantic elements in HTML
+    Semantic elements convey meaning about the structure of the document. Examples include <header>, <nav>, <article>,
+    <section>, <footer>, and others.
+ */
+
+/* Question 4: Difference between the <script> tag with the async and defer attributes.
+    <script async> loads the script asynchronously while the HTML document continues parsing.
+    <script defer> defers script execution until after the HTML document has been fully parsed.
+
+ */
+
+/* Question 5: HTML Attributes?
+    Attributes are the properties that can be added to an HTML tag. These attributes change the way the tag behaves or is
+    displayed. For example, a <img> tag has an src attribute, which you use to add the source from which the image should
+    be displayed.
+
+ */
+
+/* Question 6: Void elements in HTML
+    Void elements in HTML are tags that do not require a closing tag. They are used to insert images, line breaks, and
+    other content that does not require additional information.
+
+ */
+

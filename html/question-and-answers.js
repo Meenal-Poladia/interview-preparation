222df5e50a5b158d07fe55b02641e907e3f54ebf
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-12-29 12:08:35 -0500

    Added html interview questions

--- html/question-and-answers.js
+++ html/question-and-answers.js
@@ -33,3 +33,35 @@
 
  */
 
+/* Question 7: Role of the <meta> tag in HTML
+    The <meta> tag provides additional information about the web page, such as the author, description, and keywords.
+    It is located within the <head> section of the HTML document.
+
+ */
+
+/* Question 8: Difference between an absolute and relative URL
+    An absolute URL includes the full web address, the protocol (such as http or https) and the domain name
+    (such as www.example.com).
+
+    A relative URL, on the other hand, specifies the location of a resource relative to the current web page.
+    For example, a relative URL might include the file path (such as /images/picture.jpg) or the relative path
+    (such as ../images/picture.jpg).
+ */
+
+/* Question 9: Role of the action attribute in HTML forms
+    The action attribute is used to specify the URL of the script or program that will process the data submitted by
+    the form. When the user clicks the submit button, the form data is sent to the specified URL for processing.
+
+ */
+
+/* Question 10: Key Differences Between LocalStorage And SessionStorage Objects
+    The key differences between localStorage and sessionStorage objects are as follows:
+
+    The localStorage object stores the data without an expiry date. However, sessionStorage object stores the data for
+    only one session.
+    In the case of a localStorage object, data will not delete when the browser window closes. However, the data gets
+    deleted if the browser window closes, in the case of sessionStorage objects.
+    The data in sessionStorage is accessible only in the current window of the browser. But, the data in the
+    localStorage can be shared between multiple windows of the browser.
+
+ */

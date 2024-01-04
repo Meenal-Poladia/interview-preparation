e260efc6d909372d575a8b2535b0770cdd24f7b3
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-04 12:18:43 -0500

    Added express interview questions

---
+++ express/question-and-answers.js
@@ -0,0 +1,54 @@
+
+/* Question 1: What is Express.js
+    Express is a small framework that sits on top of Node.js’s web server functionality to simplify its APIs and add
+    helpful new features. It makes it easier to organize your application’s functionality with middleware and routing;
+    it adds helpful utilities to Node.js’s HTTP objects; it facilitates the rendering of dynamic HTTP objects.
+
+ */
+
+/* Question 2: Features of Express.js
+    Express is a lightweight, minimal and flexible Node.js web application framework, it provides a robust set of
+    features for web and mobile applications.
+
+    Few features of the Express.js includes
+    1. Routing: Express provides a simple way to define routes for handling HTTP requests. Routes are used to map
+    different URLs to specific pieces of code, making it easy to organize your application’s logic.
+    2. Middleware: Express uses middleware functions to perform tasks during the request-response cycle. Middleware
+    functions have access to the request, response, and the next middleware function.
+    3. HTTP Utility Methods: Express mainly used for handling HTTP methods like GET, POST, PUT, and DELETE. This makes
+    it easy to define how the application should respond to different types of HTTP requests.
+    4. Static File Serving: It can also serve static files, such as images, CSS, and JavaScript, with the help of
+    built-in express.static middleware.
+    5. Security: It includes features and middleware to strengthen the security of your web applications, such as the
+    helmet middleware to secure your app.
+
+ */
+
+/* Question 3: How does Express.js differ from Node.js
+    Node.js is a runtime for executing JavaScript, while Express.js is a framework built on top of Node.js to simplify
+    web development.
+
+ */
+
+/* Question 4: Is Express JS a front-end or a back-end framework
+    Express.js is a JavaScript backend framework. It is mainly designed to develop complete web applications and APIs.
+    Express is the backend component of the MERN stack which stands for MongoDB, Express.js, React.js, Node.js.
+
+ */
+
+/* Question 5: Structure of an Express JS application
+    The structure of an Express JS application can vary greatly depending on its complexity and the specific needs of
+    the project. However, here is a basic approach that is commonly used:
+
+    1. Entry point: This is the starting point of the application where you set up your server, connect to your
+    database, add middleware, and define the main routes.
+    2. Routes directory: This directory contains files for the app’s routes.
+    3. Controllers directory: This directory contains files that define the logic to handle requests for a specific
+    route.
+    4. Models directory: This directory is used for creating the schema models for the different data.
+    5. Middleware directory: This directory contains custom middleware functions that you can use in your routes.
+    6. Views directory: If you’re using a templating engine, this directory contains your view templates.
+    7. Public directory: This directory contains static files that are served directly by the server such as images, CSS
+    files, and JavaScript files.
+
+ */

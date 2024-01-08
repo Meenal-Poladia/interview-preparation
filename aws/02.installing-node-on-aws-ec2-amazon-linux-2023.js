1c03849736d9167ed2df6b1695ba80f0f1df75f5
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-08 09:35:44 -0500

    Added information on creating a server on aws

---
+++ aws/02.installing-node-on-aws-ec2-amazon-linux-2023.js
@@ -0,0 +1,13 @@
+/*
+
+    sudo yum update -y
+
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+    nvm install 20.8.0
+
+    nvm use 20.8.0
+
+    nvm alias default 20.8.0
+
+ */
---
+++ aws/03.installing-git-on-aws-ec2-amazon-linux-2023.js
@@ -0,0 +1,7 @@
+/*
+    // Install git on box
+    sudo yum install -y git
+
+    // Generate key pair for communication(clone, pull) with github account
+    ssh-keygen -t ed25519 -C "meenal.poladia94@gmail.com"
+ */

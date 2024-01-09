8f45444e58a6f64c980fbeecd228e2b6e3187e97
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-09 10:36:12 -0500

    Added dbms question and answers

---
+++ dbms/question-and-answers.js
@@ -0,0 +1,76 @@
+
+/* Question 1: What is DBMS
+    DBMS provides us an interface or tool for performing different operations such as the creation of a database,
+    inserting data into it, deleting data from it, updating the data, etc. DBMS is a software in which data is stored
+    in a more secure way as compared to the file-based system.
+
+    A DBMS system helps a user to overcome problems like data inconsistency, data redundancy, etc. in a database and
+    makes it more convenient and organized to use it.
+
+ */
+
+/* Question 2: Difference between DBMS and File System
+    A file-based system and a database system are two different approaches to managing and organizing data.
+
+    1. Data Structure:
+        - In a file-based system, data is stored in files or folders. Each file is typically specific to a particular
+        application and contains data relevant to that application.
+        - In a database system, data is organized in a structured manner using tables. The relational model is a widely
+        used approach, where data is stored in rows and columns.
+    2. Data Retrieval and Manipulation:
+        - Retrieving and manipulating data in a file-based system often involves custom programming for each
+        application.
+        - Database systems use a standardized query language such as SQL (Structured Query Language) for data retrieval
+        and manipulation.
+    3. Scalability and Maintenance:
+        - Maintenance tasks, such as adding new features or modifying existing ones, may require significant effort and
+        can be error-prone.
+        - Maintenance tasks are more straightforward in a database system, as changes can be made to the database
+        schema without affecting the entire application.
+
+ */
+
+/* Question 3: Differentiate between DBMS and RDBMS
+DBMS                                                        RDMS
+- DBMS stores data as file.                                 - RDBMS stores data in tabular form.
+- Data elements need to access individually.                - Multiple data elements can be accessed at the same time.
+- No relationship between data.                             - Data is stored in the form of tables which are related to
+                                                            each other.
+- Normalization is not present.                             - Normalization is present.
+- Data redundancy is common in this model.	                - Keys and indexes do not allow Data redundancy.
+- It is used for small organization and deal with small     - It is used to handle large amount of data.
+data.
+- Data fetching is slower for the large amount of data.     - Data fetching is fast because of relational approach.
+- It stores data in either a navigational or hierarchical   - It uses a tabular structure where the headers are the
+form.                                                         column names, and the rows contain corresponding values.
+
+
+ */
+
+/* Question 4: What is Database
+    A Database is a logical, consistent and organized collection of data that it can easily be accessed, managed and
+    updated.
+    Database mostly consists of the objects (tables), and tables include of the records and fields. Fields are the
+    basic units of data storage, which contain the information about a particular aspect or attribute of the entity
+    described by the database. DBMS is used for extraction of data from the database in the form of the queries.
+
+ */
+
+/* Question 5: What is Database System
+    The collection of database and DBMS software together is known as a database system. Through the database system,
+    we can perform many activities such as-
+    The data can be stored in the database with ease, and there are no issues of data redundancy and data inconsistency.
+
+ */
+
+
+
+/* Notes:
+    - Data Redundancy: Data redundancy is when multiple copies of the same information are stored in more than one place
+    at a time.
+
+    - Normalisation: Normalization is the process of organizing data in a database. It includes creating tables and
+    establishing relationships between those tables according to rules designed both to protect the data and to make
+    the database more flexible by eliminating redundancy and inconsistent dependency.
+
+ */

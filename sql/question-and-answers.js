bdbf3b912d9b9f419f8af791627553cb87aceab4
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-10 11:14:23 -0500

    Added sql question and answers

---
+++ sql/question-and-answers.js
@@ -0,0 +1,113 @@
+
+/* Question 1: What is SQL
+    SQL is a programming language which is used to communicate with the database. SQL is used to define the database
+    structure and it is used to manipulate the data within.
+
+    SQL stands for Structured Query Language whose main purpose is to interact with the relational databases in the
+    form of inserting, deleting and updating/modifying the data in the database.
+
+ */
+
+/* Question 2: Relationship in database
+    In web everything is related to each other. While creating database we can create relationship between tables so
+    that they are related to each other in some way. Relationship talk about entities.
+    A Relationship in DBMS depicts an association between the tables.
+
+    There can be 3 types of relationships:
+        1. One to One
+        2. One to Many
+        3. Many to Many
+
+ */
+
+/* Question 3: Types of Relationships
+    1. One-to-one:
+    One entity has connection with one another entity. Used when a single row in Table A is related to a single row in
+    Table B.
+    For example: One person can have just one SIN number at a time.
+
+    2.One-to-many:
+    Used when a single row in Table A is related to many rows in table B.
+    For example - On youtube reviews, one individual can write multiple comments but the comments belongs to only one
+    person at a time.
+    Another example is one king having many wives but those wives are not allowed to marry any other person.
+
+    3. Many-to-many
+    Used when many rows in table A can be related to many rows in table B.
+
+    4. Self-Referencing relationship:
+    Self -Referencing Relationship – Used when a record in table A is related to the same table itself.
+
+ */
+
+/* Question 4: Parent and Child Table
+    Keys connect different tables and different tables have keys which are unique to the table. What is primary key
+    in one table might be a foreign key in another table.
+
+    Primary key is with the parent and foreign key is with the child. The child inherits the values from the parent.
+
+    In many to many relationship we have problem with the parent child relationship so we cannot store it in database.
+
+ */
+
+/* Question 5: Introduction to Keys
+    Keys keeps everything unique. Something unique in each row which differentiates it from another is called as a key.
+    For example - email id of each user will be different from one another so there is unique value in each row.
+
+    Two similar keys should not exist in the database. Keys can be a combination of two columns.
+
+    Key should never be changed. If the key of one table changes then we need to update the key in other connecting
+    tables as well.
+
+    Keys can never be null.
+
+    3 important things about keys:
+        1. Keys should always be unique
+        2. Keys should never be changing
+        3. Keys should never be null
+
+ */
+
+/* Question 6: Types of Keys
+    1. Primary Key:
+    A primary key is an attribute or a set of attributes that uniquely identify each record in the table. A table can
+    have duplicate columns, but it cannot have more than one primary key. Primary Key cannot be empty or null. If the
+    primary key is changed then is should be changed across the database.
+
+    2. Super Key:
+    Superkey is any number of columns that forces the row to be unique. So, a candidate key, primary key, and a unique
+    key is a superkey, but vice-versa isn’t true. Superkey can have many cnadidate keys and other redundant keys as
+    well.
+    For knowing if the column is a superkey we ask the question:
+        1. Can every row be unique?
+        2. How many columns are needed?
+        3. How many CK do we have?
+
+    3. Candidate Key:
+    Candidate key is the least number of columns needed to force every row to be unique. Candidate keys are candidates
+    for making a primary key.
+
+    4. Unique Key:
+    The unique key is similar to the primary key, but allows NULL values in the column.
+
+    5. Foreign Key:
+    Foreign key is a reference to a primary key. We can have multiple foreign keys in a table.
+    Multiple foreign keys have connection to different table. Each column can have one reference.
+
+    6. Alternate Key:
+    Alternate keys are all the candidate keys that were not selected as primary keys. Alternate keys are not must but
+    it is good to have them as they can help in searches, or we can create an index.
+
+    7. Composite Key:
+    A composite key is a combination of two or more columns that identify each tuple uniquely.
+
+    8. Natural key and Surrogate key:
+    Surrogate and natural keys are categories of primary keys.
+
+    Natural keys are something we want to store naturally in the table. To add a natural key we do not have to make up
+    something or add an extra  for example id.
+
+    Surrogate keys are made up keys. They are added in our column. Surrogate keys are kept very private and no one
+    knows them except for the people working with them.
+
+ */

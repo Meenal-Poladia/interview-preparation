590377f8cbb04b9dbde9b78a0e1f372acd17e30e
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-14 10:46:55 -0500

    Added git question and answers

---
+++ git/question-and-answers.js
@@ -0,0 +1,33 @@
+
+/* Question 1: What is Git
+    Git is a distributed version control system that helps track changes in source code during software development. It
+    allows multiple developers to collaborate on a project, manage versions, and track changes efficiently.
+
+ */
+
+/* Question 2: What is a version control system (VCS)
+    A VCS keeps track of the contributions of the developers working as a team on the projects. They maintain the
+    history of code changes done and with project evolution, it gives an upper hand to the developers to introduce
+    new code, fixes bugs, and run tests with confidence that their previously working copy could be restored at any
+    moment in case things go wrong.
+
+ */
+
+/* Question 3: Explain the difference between Git and GitHub
+    Git is the version control system, while GitHub is a web-based platform that provides hosting for Git repositories.
+    GitHub adds a social and collaborative layer on top of Git, making it easier for multiple developers to work
+    together on a project.
+
+ */
+
+/* Question 4: What is a Git repository
+    A Git repository is a folder that contains all the files and version history of a project. It includes a hidden
+    folder named ".git" that stores metadata, branches, and the complete history of the project.
+
+ */
+
+/* Question 5: How do you create a new branch in Git
+     To create a new branch, you can use the following command:
+     -> git checkout -b new-branch-name
+
+ */

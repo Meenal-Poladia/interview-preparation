46123a695d9e8f55129cb43201864207bb3fb554
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-14 10:51:14 -0500

    Added git question and answers

--- git/question-and-answers.js
+++ git/question-and-answers.js
@@ -31,3 +31,38 @@
      -> git checkout -b new-branch-name
 
  */
+
+/* Question 6: Explain the difference between 'git pull' and 'git fetch.'
+    git pull fetches changes from the remote repository and automatically merges them into the current branch.
+
+    On the other hand, git fetch fetches changes but doesn't automatically merge them, allowing you to review changes
+    before merging.
+
+ */
+
+/* Question 7: What is a Git merge conflict, and how do you resolve it
+    A merge conflict occurs when Git cannot automatically merge changes from different branches. To resolve it, you
+    need to manually edit the conflicting files, choose which changes to keep, and then commit the resolved files.
+
+ */
+
+/* Question 8: How do you revert a commit in Git
+    To revert the last commit, you can use: git revert HEAD
+
+    If you want to revert a specific commit, you can use: git revert <commit-hash>
+
+ */
+
+/* Question 9: Explain the purpose of Git's 'stash' command
+    The git stash command is used to save changes that are not ready to be committed but need to be set aside
+    temporarily. It allows you to switch to another branch or perform other operations without committing the current
+    changes.
+
+ */
+
+/* Question 10: What is the Gitflow workflow
+    Gitflow is a branching model that defines specific branches and their purposes. It typically includes branches
+    like master for production releases, develop for ongoing development, and feature branches for new features. It
+    helps organize the development process in larger projects.
+
+ */

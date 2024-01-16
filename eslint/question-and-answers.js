09471c237ba95982967eb71fe4b7e1ba29fe27c6
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-16 10:19:22 -0500

    Added eslint question and answers

---
+++ eslint/question-and-answers.js
@@ -0,0 +1,33 @@
+
+/* Question 1: What is a linter
+    Linters highlights syntactical and stylistic problems in your source code, which often times helps you identify and
+    correct subtle programming errors or unconventional coding practices that can lead to errors.
+
+ */
+
+/* Question 2: What is ESLint, and why is it used
+     ESLint is a static code analysis tool for identifying and fixing problems in JavaScript code. It is used to
+     enforce coding style consistency, catch common programming errors, and maintain a high code quality standard in a
+     codebase.
+
+ */
+
+/* Question 3: Explain the concept of ESLint rules
+    ESLint rules define the coding style and guidelines that ESLint enforces. Rules can be configured in the .eslintrc
+    file and can cover areas such as indentation, variable naming, syntax preferences, and more.
+
+ */
+
+/* Question 4: What is the purpose of the .eslintignore file
+    The .eslintignore file is used to specify files and directories that ESLint should ignore during linting. It
+    allows you to exclude certain files or patterns from being analyzed.
+
+ */
+
+/* Question 5: How do you fix ESLint errors automatically
+    ESLint can automatically fix certain types of errors using the --fix option.
+
+    For example:
+    eslint --fix file.js
+
+ */

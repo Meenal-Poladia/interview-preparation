cc95a2220fed82fbbf74ba2bf4427e884a045ba9
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-16 10:57:58 -0500

    Added webpack question and answers

---
+++ webpack/question-and-answers.js
@@ -0,0 +1,36 @@
+
+/* Question 1: What is Webpack, and what problems does it solve
+    Webpack is a popular module bundler for javascript application.
+    Webpack is a module bundler that takes modules with dependencies and generates static assets representing those
+    modules. It solves the problem of managing and bundling numerous JavaScript, CSS, and other files into a smaller
+    set of files for efficient delivery to browsers.
+
+ */
+
+/* Question 2: Explain the main concepts of Webpack
+    - Entry: The entry point of the application where Webpack starts bundling usually index.js.
+    - Output: The result of the bundling process, typically a single or multiple files.
+    - Loader: Modules that transform or process files before adding them to the bundle.
+    - Plugin: Modules that perform tasks such as code optimization, asset management, and more. For example: uglify.
+    - Module: Any file, including JavaScript, CSS, or images, that Webpack can understand and process.
+
+ */
+
+/* Question 3: What is the purpose of loaders in Webpack
+    Loaders in Webpack are used to process files before they are added to the bundle. They transform file content,
+    apply transformations (e.g., Babel for JavaScript), or load assets like images and fonts.
+
+ */
+
+/* Question 4: How do you configure Webpack
+     Webpack is configured using a JavaScript configuration file (usually named webpack.config.js). This file specifies
+     the entry point, output configuration, and various loaders and plugins.
+
+ */
+
+/* Question 5: Explain the role of the webpack-dev-server
+    webpack-dev-server is a development server for Webpack that provides features like live reloading, hot module
+    replacement, and a simple HTTP server. It allows developers to see changes in real-time while developing without
+    manually refreshing the browser.
+
+ */

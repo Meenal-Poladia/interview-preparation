727c76cf90d0374cfd14ccfc29612979af5ede06
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-20 18:28:25 -0500

    Updated the aws deployment process

---
+++ aws/deployment-on-single-EC2-free-tier-instance/007.viewing-logs-in-amazon-linux.js
@@ -0,0 +1,12 @@
+/*
+    Views the latest 50 logs
+    sudo journalctl -n 50 -u pomodoro-timer.service
+
+    Views all logs
+    journalctl -u pomodoro-timer.service
+
+    To view logs as they are generated
+    sudo journalctl -f -u pomodoro-timer.service
+
+
+ */

84791dd32cc174818adebe099a3bf870759d7b0b
Meenal-Poladia <meenal.poladia94@gmail.com>
2024-01-21 22:14:05 -0500

    Updated the process for aws deployment

---
+++ aws/deployment-on-single-EC2-free-tier-instance/006.adding-ssl-certificate.js
@@ -0,0 +1,14 @@
+/*
+    Resource Link:
+    https://stackoverflow.com/questions/73347341/cannot-download-caddy-package-on-yum
+    https://nip.io/
+    http://www.asheiduk.de/post/install-caddy-on-amazon-linux-2/
+    https://search.nixos.org/packages?channel=unstable&show=caddy&query=caddy
+    https://bansalanuj.com/https-aws-ec2-without-custom-domain
+    https://stackoverflow.com/questions/53824640/nginx-lets-encrypt-could-not-automatically-find-a-matching-server-block
+    https://community.letsencrypt.org/t/the-certificate-was-saved-but-could-not-installed-installer-nginx/153645
+    https://community.letsencrypt.org/t/certbot-with-nip-io-or-xip-io/142262/10
+    https://learnaws.io/blog/caddy-ec2-tutorial
+
+
+ */
